cc80ce01c44fe6048b9219608a9a8eb9135c9acc
Isam Balghari <isamabdullah88@gmail.com>
2025-10-15 11:08:14 +0500

    Cleanup.

--- train.py
+++ train.py
@@ -18,16 +18,12 @@ def prepbatch(batch, numatoms, device):
         ztpadded = F.pad(zt, (0, numatoms-n), value=0.0)
         zblocks.append(ztpadded)
         
-    # zbatch = pad_sequence(zblocks, batch_first=True)
     zbatch = torch.stack(zblocks, dim=0)
-    # print('zbatch: ', zbatch.size())
-    # print('pos: ', batch.pos.size())
     posbatch = torch.zeros(bsize, numatoms, 3).to(device)
     for i in range(bsize):
         idx = batch.batch == i
         posbatch[i, :idx.sum(), :] = batch.pos[idx, :]
 
-    # print('posbatch: ', posbatch.size())
     dist = torch.cdist(posbatch, posbatch)
 
     return zbatch, dist, batch.y[:, 7]
@@ -60,35 +56,9 @@ def train():
             batch = batch.to(device)
             optimizer.zero_grad()
             
-            # print('bsize: ', bsize)
-            # n = batch.pos.size(0)
-            
-            
-            # zblocks = []
-            # for i in range(bsize):
-            #     zt = batch.z[batch.batch == i]
-            #     n = zt.size(0)
-            #     ztpadded = F.pad(zt, (0, numatoms-n), value=0.0)
-            #     zblocks.append(ztpadded)
-                
-            # # zbatch = pad_sequence(zblocks, batch_first=True)
-            # zbatch = torch.stack(zblocks, dim=0)
-            # # print('zbatch: ', zbatch.size())
-            # # print('pos: ', batch.pos.size())
-            # posbatch = torch.zeros(bsize, numatoms, 3).to(device)
-            # for i in range(bsize):
-            #     idx = batch.batch == i
-            #     posbatch[i, :idx.sum(), :] = batch.pos[idx, :]
-
-            # # print('posbatch: ', posbatch.size())
-            # dist = torch.cdist(posbatch, posbatch)
-            # print('dist: ', dist.size())
-            # print('y: ', batch.y[:, 7].size())
             zbatch, dist, target = prepbatch(batch, numatoms, device)
             pred = model(zbatch, dist)
             loss = criterion(pred, target)
-
-            # loss = loss / batch_size
 
             loss.backward()
             optimizer.step()
